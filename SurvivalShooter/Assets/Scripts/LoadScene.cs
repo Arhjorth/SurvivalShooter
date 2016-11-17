9848561e94b64a542c7f003e7e5ae49377c97bb9
welwa <welwa-knudsen@hotmail.com>
2016-11-17 13:55:04 +0100

    healthUI and player hit animation

---
+++ SurvivalShooter/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+    public void LoadScenes()
+    {
+        Application.LoadLevel("Lvl01");
+    }
+}
--- SurvivalShooter/Assets/Scripts/PlayerHealth.cs
+++ SurvivalShooter/Assets/Scripts/PlayerHealth.cs
@@ -1,43 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
     public int startingHealth = 100;            // The amount of health the enemy starts the game with.
     private int currentHealth;
+    public Slider healthSlider;
+    public Image dmgImage;
+    public float flashSpeed = 5f;
+    public Color flashcolour = new Color(1f, 0f, 0f, 0.1f);
     bool isDead;
-    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+    bool damaged;
 
+    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
     PlayerController playerController;
-    ShootController shootController; 
+    ShootController shootController;
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         currentHealth = startingHealth;
         capsuleCollider = GetComponent<CapsuleCollider>();
         playerController = GetComponent<PlayerController>();
         shootController = GetComponentInChildren<ShootController>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            dmgImage.color = flashcolour;
+
+        }
+        else
+        {
+            dmgImage.color = Color.Lerp(dmgImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
 
 
-    public void takeDamage(int amount) {
+    public void takeDamage(int amount)
+    {
         // If the enemy is dead...
         if (isDead)
             // ... no need to take damage so exit the function.
             return;
 
+        damaged = true;
         // Play the hurt sound effect.
         //enemyAudio.Play ();
 
         // Reduce the current health by the amount of damage sustained.
         currentHealth -= amount;
 
+        healthSlider.value = currentHealth;
         // Set the position of the particle system to where the hit was sustained.
         //	Debug.Log(hitParticles.transform.position);
         //	Debug.Log(hitPoint);
@@ -47,13 +68,15 @@ public class PlayerHealth : MonoBehaviour {
         //	hitParticles.Play();
 
         // If the current health is less than or equal to zero...
-        if (currentHealth <= 0) {
+        if (currentHealth <= 0)
+        {
             // ... the enemy is dead.
             Death();
         }
     }
 
-    private void Death() {
+    private void Death()
+    {
         isDead = true;
 
         // Turn the collider into a trigger so shots can pass through it.
@@ -62,7 +85,8 @@ public class PlayerHealth : MonoBehaviour {
         playerController.enabled = false;
         shootController.enabled = false;
 
-        foreach(Renderer r in GetComponentsInChildren<Renderer>()) {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
             r.enabled = false;
         }
 
