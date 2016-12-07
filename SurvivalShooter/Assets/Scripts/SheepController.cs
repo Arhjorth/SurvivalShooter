59095b382c55b3aa70b034ede686939fb13f78e8
Andreas Hjorth <arhjorth@hotmail.com>
2016-12-07 23:16:21 +0100

    Sheep follows when f is pushed in range

--- SurvivalShooter/Assets/Scripts/SheepController.cs
+++ SurvivalShooter/Assets/Scripts/SheepController.cs
@@ -5,15 +5,42 @@ public class SheepController : MonoBehaviour {
 
     Transform playerTransform;
     NavMeshAgent nav;
-
+    private bool playerInRange;
+    private bool followPlayer;
 
     void Awake() {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<NavMeshAgent>();
+        playerInRange = false;
+        followPlayer = false;
     }
 
     // Update is called once per frame
     void Update() {
-        nav.SetDestination(playerTransform.position);
+        if (Input.GetKeyDown(KeyCode.F) && playerInRange ) {
+            if (followPlayer) {
+                followPlayer = false;
+            }
+            else if (!followPlayer) {
+                followPlayer = true;
+            }
+        }
+
+        if (followPlayer) {
+            nav.SetDestination(playerTransform.position);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            playerInRange = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            Debug.Log("PLAYER IN RANGE");
+            playerInRange = true;
+        }
     }
 }
