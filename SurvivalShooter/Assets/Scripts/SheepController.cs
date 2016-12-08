5c1cbef3ebd79301f6f27d6090be741fdc471693
Andreas Hjorth <arhjorth@hotmail.com>
2016-12-08 12:43:36 +0100

    Wandering (random walk) will not work

--- SurvivalShooter/Assets/Scripts/SheepController.cs
+++ SurvivalShooter/Assets/Scripts/SheepController.cs
@@ -9,13 +9,15 @@ public class SheepController : MonoBehaviour {
     private bool followPlayer;
     private bool sheepInSafeZone;
     public Plane safeZone;
-
+    //int safeZoneMesh;
     void Awake() {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<NavMeshAgent>();
         playerInRange = false;
         followPlayer = false;
         sheepInSafeZone = false;
+        //safeZoneMesh = NavMesh.GetNavMeshLayerFromName("Safezone");
+
     }
 
     // Update is called once per frame
@@ -36,8 +38,10 @@ public class SheepController : MonoBehaviour {
                 nav.SetDestination(playerTransform.position);
             }
         }
-        else {
-        }    
+       // else { // Try to get randompoint to work
+            //Vector3 rp = RandomPoint(transform.position, 10, safeZoneMesh);
+       //     nav.SetDestination(rp);
+       // }    
     }
 
     void OnTriggerExit(Collider other) {
@@ -53,7 +57,18 @@ public class SheepController : MonoBehaviour {
     }
 
     public void setSheepInSafeZone() {
-        Debug.Log("SheepController - setSheepInSafeZone");
         sheepInSafeZone = true;
+    }
+
+
+    Vector3 RandomPoint(Vector3 center, float range, int meshIndex) {
+        Vector3 hitpos = Vector3.zero;
+        for (int i = 0; i < 30; i++) {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, meshIndex))
+                hitpos = hit.position;
+         }
+        return hitpos;
     }
 }
