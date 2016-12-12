8faf51e8e2a1bd97bc5196626b13c961fb29fcc1
Andreas Hjorth <arhjorth@hotmail.com>
2016-12-12 17:13:39 +0100

    Sheepspawner implemented

---
+++ SurvivalShooter/Assets/Scripts/SheepSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepSpawner : MonoBehaviour {
+
+    public GameObject Sheep;
+    GameObject[] spawnPlaces;
+    GameObject safeZone;
+    SafezoneController safeZoneInfo;
+
+	// Use this for initialization
+	void Awake () {
+        spawnPlaces = GameObject.FindGameObjectsWithTag("SpawnSheep");
+
+        safeZone = GameObject.FindGameObjectWithTag("Safezone");
+        safeZoneInfo = safeZone.GetComponent<SafezoneController>();
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int numberOfSheeps = GameObject.FindGameObjectsWithTag("Sheep").Length;
+        int sheepsInPasture = safeZoneInfo.getNumberOfSheeps();
+        
+        if (numberOfSheeps - sheepsInPasture == 0) {
+            GameObject rndSpawn = spawnPlaces[Random.Range(0, spawnPlaces.Length - 1)];
+            GameObject instance = (GameObject)Instantiate(Sheep, rndSpawn.transform.position, Quaternion.identity);
+        }	
+	}
+}
